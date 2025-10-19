f415cd9a94b801b7dbc5d8e37b6438dabfb62d0f
S23B0121-AqifAddin <s23b0121@siswa.umk.edu.my>
2025-10-19 14:50:10 +0800

    Update tuto1.py

--- tuto1.py
+++ tuto1.py
@@ -15,6 +15,13 @@ st.set_page_config(
 # Page header
 st.header("Tutorial Scientific Visualization", divider="grey")
 
+col1, col2, col3, col4 = st.columns(4)
+    
+col1.metric(label="PLO 2", value=f"3.3", help="PLO 2: Cognitive Skill", border=True)
+col2.metric(label="PLO 3", value=f"3.5", help="PLO 3: Digital Skill", border=True)
+col3.metric(label="PLO 4", value=f"4.0", help="PLO 4: Interpersonal Skill", border=True)
+col4.metric(label="PLO 5", value=f"4.3", help="PLO 5: Communication Skill", border=True)
+
 # Load your data
 try:
     df2 = pd.read_csv('https://raw.githubusercontent.com/S23B0121-AqifAddin/tuto-SV/refs/heads/main/student_survey_exported%20(1).csv', encoding='utf-8')
