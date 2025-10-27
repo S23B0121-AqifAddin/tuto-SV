9456ee8ed5fb9f66bdb029601ba5ce60930fda7e
S23B0121-AqifAddin <s23b0121@siswa.umk.edu.my>
2025-10-28 03:37:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import streamlit as st
 
+# --- Page config ---
 st.set_page_config(
-    page_title="Student Survey"
+    page_title="Accident Motorbike in Bangladesh",
+    page_icon=":material/school:",
 )
 
-visualise = st.Page('tuto1.py', title='Pencapaian Akademik Pelajar', icon=":material/school:")
+# --- Define pages ---
+home = st.Page("home.py", title="Homepage", icon=":material/home:", default=True)
+visualise = st.Page("home1.py", title="Pencapaian Akademik Pelajar", icon=":material/school:")
+page_1 = st.Page("page1.py", title="Page 1", icon=":material/description:")
+page_2 = st.Page("page2.py", title="Page 2", icon=":material/bar_chart:")
+page_3 = st.Page("page3.py", title="Page 3", icon=":material/settings:")
 
-home = st.Page('home.py', title='Homepage', default=True, icon=":material/home:")
+# --- Navigation menu ---
+pg = st.navigation({
+    "Main Menu": [home, visualise, page_1, page_2, page_3]
+})
 
-pg = st.navigation(
-        {
-            "Menu": [home, visualise]
-        }
-    )
-
+# --- Run selected page ---
 pg.run()
